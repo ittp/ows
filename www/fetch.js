11d656f301c12150a84986305b157a370c5dd709
ittp <61617660+ittp@users.noreply.github.com>
2021-11-21 11:49:16 +0300

    Create fetch.js

---
+++ www/fetch.js
@@ -0,0 +1,16 @@
+const url = 'https://userapi.vdsina.ru/v1/auth';
+const data = { email: 'tp@ittps.ru', password: '' };
+
+try {
+  const response = await fetch(url, {
+    method: 'POST', 
+    body: JSON.stringify(data), 
+    headers: {
+      'Content-Type': 'application/json'      
+    }
+  });
+    const json = await response.json();
+    console.log('Success:', JSON.stringify(json));
+} catch (error) {
+    console.error('Error:', error);
+}
