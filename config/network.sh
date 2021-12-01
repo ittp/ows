dda1a032cbace3f46ff07993cd191d15345d444a
ittp <61617660+ittp@users.noreply.github.com>
2021-12-01 12:03:31 +0300

    Update network.sh

--- config/network.sh
+++ config/network.sh
@@ -1,3 +1,41 @@
 #!/bin/sh
 
-uci show wireless
+uci export network > /tmp/network.uci.conf
+
+local proto=${1}
+
+static() {
+# $proto $ip $gw $dns
+if [ $proto == 'static'];
+then
+  iface=${1}
+  ip=${2}
+  gw=${3} 
+  dns=${4}
+  
+  static $iface $ip $gw $dns
+fi;
+
+
+
+uci batch <<-EOF
+  
+  
+  
+	set network.$iface.proto=$proto
+  set network.$iface.ipaddr=$ip
+  set network.$iface.gateway=$gw
+  set network.$iface.dns=$dns
+  
+  changes
+  
+EOF
+}
+
+wan() {
+ 
+echo "setup wan"
+
+echo $proto
+
+}
