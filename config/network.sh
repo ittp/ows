ecc7704d56f9436351cdcfeb7d4d0705ff5ab40b
ittp <61617660+ittp@users.noreply.github.com>
2021-12-01 12:06:49 +0300

    Update network.sh

--- config/network.sh
+++ config/network.sh
@@ -9,10 +9,10 @@ static() {
 # $proto $ip $gw $dns
 if [ $proto == 'static'];
 then
-  iface=${1}
-  ip=${2}
-  gw=${3} 
-  dns=${4}
+  iface=${2}
+  ip=${3}
+  gw=${4} 
+  dns=${5}
   
   static $iface $ip $gw $dns
 fi;
@@ -23,7 +23,7 @@ uci batch <<-EOF
   
   
   
-	set network.$iface.proto=$proto
+   set network.$iface.proto=$proto
   set network.$iface.ipaddr=$ip
   set network.$iface.gateway=$gw
   set network.$iface.dns=$dns
