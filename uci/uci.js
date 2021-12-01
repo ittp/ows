8be9bb48937e0b9dea9ee4be608dd7aebbd6114a
ittp <61617660+ittp@users.noreply.github.com>
2021-12-01 07:42:57 +0300

    Create uci.js

---
+++ uci/uci.js
@@ -0,0 +1,19 @@
+
+const get = (p) => {
+  console.log('get')
+}
+const show = (p) => {
+  console.log('show')
+}
+const batch = (p) => {
+  console.log('batch')
+}
+
+const uci = async (params) => {
+  
+  console.log('uci')
+ 
+  
+  
+  
+}
